20daef5d0874ed1fd4510d234a4f2a2d3c4148d5
Sergiej88 <sergey88gavrilik@gmail.com>
2022-07-11 20:37:34 +0200

    commit empty project

---
+++ src/main/java/pl/example/ItemDonationService/ItemDonationServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.example.ItemDonationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemDonationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemDonationServiceApplication.class, args);
+	}
+
+}
